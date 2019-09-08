f861e697ed1186fbd0eacfdc0b551387ba64aa22
kiranov <43752627+kiranov@users.noreply.github.com>
2019-09-08 14:04:57 +0300

    Create func_D.h

---
+++ func_D.h
@@ -0,0 +1,97 @@
+    #pragma once
+    #include <iostream>
+    #include <map>
+    #include <set>
+    #include <string>
+    #include <vector>
+    #include <fstream>
+    #include "json.hpp"
+
+    using json = nlohmann::json;
+    using namespace std;
+
+    /*enum class Action{
+        start,
+        pickup,
+        dropoff,
+        finish
+    };*/
+
+    class Event{
+        public:
+        int courier_id;
+        string action;
+        int order_id;
+        int point_id;
+        Event(int c_id, string act, int ord, int point);
+        bool operator<(const Event& right) const {
+            return order_id < right.order_id;
+        }
+        bool operator==(const Event& right) const {
+            return (courier_id == right.courier_id && order_id == right.order_id && 
+            action == right.action && point_id == right.point_id);
+        }
+    };
+
+    class Data{
+        public:
+        Event event;
+        int ferment;
+    };
+
+    class segments{
+        public:
+        map<Event, vector<Data>> segm;
+    };
+
+    class Way{
+        public:
+        vector<Event> ways;
+        //new_event должен генерировать путь, новый Event класть в вектор(с учетом вероятностой функции)
+        void new_event(int n, set<Event> );
+    };
+//вроде не нужен
+    class AllEvents{
+        public:
+        vector<vector<Event>> evs;
+    };
+
+    class courier{
+        public:
+        int courier_id;
+        int location_x;
+        int location_y;
+    };
+
+    class order{
+        public:
+        int order_id;
+        int pickup_point_id;
+        int pickup_location_x;
+        int pickup_location_y;
+        int pickup_from;
+        int pickup_to;
+        int dropoff_point_id;
+        int dropoff_location_x;
+        int dropoff_location_y;
+        int dropoff_from;
+        int dropoff_to;
+        int payment;
+
+    };
+
+    class depot{
+        public:
+        int point_id;
+        int location_x;
+        int location_y;
+    };
+
+    vector<vector<Event>> possible_events(vector<courier> cour, vector<order> ord, vector<depot> dep);
+
+    //нужно переписать функцию с питона
+    void cheсker();
+
+    Event GetNextEvent();
+
+   
